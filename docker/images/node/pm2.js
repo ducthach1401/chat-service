780c749814407a3205cc274f7c9436b2b78a0a42
ducthach <ducthach.1401@gmail.com>
2022-10-03 08:34:54 +0000

    update

--- docker/images/node/pm2.js
+++ docker/images/node/pm2.js
@@ -4,6 +4,8 @@ module.exports = {
       name: 'app',
       cwd: '/var/www',
       script: './dist/main.js',
+      exec_mode: process.env.PM2_EXEC_MODE,
+      instances: process.env.PM2_INSTANCES,
     },
   ],
 };
